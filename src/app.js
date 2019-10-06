e3a19df9627c881f5b9a7501050334c6a5979ca1
Rinat <rinat94ua@gmail.com>
2019-10-06 13:44:06 +0300

    Added dynamic import with @babel/plugin-syntax-dynamic-import, React.lazy and React.Suspense

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import React from 'react';
 import '@babel/polyfill';
 import {hot} from 'react-hot-loader';
 
+const Warning =   React.lazy(() => require('./Warning'))
+
 class App extends React.Component {
   render() {
-    return <h1>Hello Hello</h1>
+    return (
+    <>
+      <h1>Hello Hello</h1>
+      <React.Suspense fallback={null}>
+        <Warning />
+      </React.Suspense>
+    </>)
   }
 }
 

f9cf7e72ac0b2e619c5538271eba57cba0001b06
Rinat <rinat94ua@gmail.com>
2019-10-06 10:54:08 +0300

    Added css-plugins

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,9 +14,17 @@ module.exports = {
       loader: 'babel-loader',
       exclude: /node_modules/,
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react']
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: [
+          '@babel/plugin-proposal-class-properties'
+        ]
       }
-    }]
+    },
+  {
+    test: /\.css$/,
+    use: ['style-loader','css-loader'],
+    exclude: /node_modules/
+  }]
   },
   plugins: [
     new HTMLWebpackPlugin({
